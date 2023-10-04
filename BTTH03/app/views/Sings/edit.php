0b69a0e1451269f123f6b9a4bcf770ffe4e6e00c
LewwDinhHanh <minhnt0109@gmail.com>
2023-10-04 07:41:26 +0700

    up

---
+++ BTTH03/app/views/Sings/edit.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container border border-success mt-3">
+        <div class="row">
+            <div class="col-md">
+                <form class="mt-2">
+                    <span class="text-success fs-1">Sửa Bài Hát</span>
+                    <div class="mb-3">
+                        <label class="form-label">Tên Bài Hát</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Ca Sĩ</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">ID Thể Loại</label>
+                        <input type="text" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary m-2">Sửa</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
